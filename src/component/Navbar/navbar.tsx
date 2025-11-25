52b9c145c0e12e1516213ff61fcc37532f6615fb
code-farmer-VII <temesgengonfa72127@gmail.com>
2025-11-24 23:09:54 -0800

    I change the time is of the essence to time is a key

--- src/component/Navbar/navbar.tsx
+++ src/component/Navbar/navbar.tsx
@@ -50,7 +50,7 @@ export default function Navbar() {
           />
 
           <div className="text-xl font-bold leading-tight">
-            Time is of the essence
+            Time is key essence
             <div className="h-[2px] bg-orange-500 mt-1" />
             Freight Logistics
           </div>
@@ -108,7 +108,7 @@ export default function Navbar() {
         <div className="flex items-center gap-2 mb-8 text-black">
           <Image src={logoLight} alt="logo" width={80} height={80} />
           <div className="font-bold">
-            Time is of the essence
+            Time is key essence
             <div className="h-[2px] bg-orange-500" />
             Freight Logistics
           </div>
